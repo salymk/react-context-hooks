b03a43fa9fc3b17c85758f819e6a9062b3e2c692
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-11 07:48:28 -0700

    Provided context to booklist

--- context-app/src/components/BookList.js
+++ context-app/src/components/BookList.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
-	state = {};
+	static contextType = ThemeContext;
 	render() {
+		const { isLightTheme, light, dark } = this.context;
+		const theme = isLightTheme ? light : dark;
 		return (
-			<div className='book-list'>
+			<div
+				className='book-list'
+				style={{ color: theme.syntax, background: theme.bg }}>
 				<ul>
-					<li>The way of kings</li>
-					<li>The of the wind</li>
-					<li>The final empire</li>
+					<li style={{ background: theme.ui }}>The way of kings</li>
+					<li style={{ background: theme.ui }}>The of the wind</li>
+					<li style={{ background: theme.ui }}>The final empire</li>
 				</ul>
 			</div>
 		);
