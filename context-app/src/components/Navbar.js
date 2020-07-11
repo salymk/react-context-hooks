cf4040f4b999fcf322e3072e19f218fcaa7b8a8c
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-11 07:37:11 -0700

    Style navbar based on theme value

--- context-app/src/components/Navbar.js
+++ context-app/src/components/Navbar.js
@@ -7,8 +7,10 @@ class Navbar extends Component {
 	render() {
 		console.log(this.context);
 		const { isLightTheme, light, dark } = this.context;
+		const theme = isLightTheme ? light : dark;
+
 		return (
-			<nav>
+			<nav style={{ background: theme.ui, color: theme.syntax }}>
 				<h1>Context App</h1>
 				<ul>
 					<li>Home</li>
