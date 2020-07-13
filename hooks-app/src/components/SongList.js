4258f20d0f951553c840457731878e8f96e18057
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-13 06:23:55 -0700

    Added hooks

--- hooks-app/src/components/SongList.js
+++ hooks-app/src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import uuid from 'uuid/v1';
 import NewSongForm from './NewSongForm';
 
@@ -13,6 +13,11 @@ const SongList = () => {
 		setSongs([...songs, { title, id: uuid() }]);
 	};
 
+	const [age, setAge] = useState(20);
+
+	useEffect(() => {
+		console.log('useEffect hook ran', songs);
+	}, [songs]);
 	return (
 		<div>
 			<div className='song-list'>
@@ -22,6 +27,7 @@ const SongList = () => {
 					})}
 				</ul>
 				<NewSongForm addSong={addSong} />
+				<button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
 			</div>
 		</div>
 	);
