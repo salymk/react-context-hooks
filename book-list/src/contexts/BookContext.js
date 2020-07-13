833404c2808e2dc044c91d2d23678828ee92b933
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-13 07:29:15 -0700

    Created BookContext

--- book-list/src/contexts/BookContext.js
+++ book-list/src/contexts/BookContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react';
+import uuid from 'uuid/v1';
 
 export const BookContext = createContext();
 
@@ -7,4 +8,18 @@ const BookContextProvider = (props) => {
 		{ title: 'name of the wind', author: 'patrick rothfuss', id: 1 },
 		{ title: 'the final empire', author: 'brandon sanderson', id: 2 },
 	]);
+
+	const addBook = (title, author) => {
+		setBooks([...books, { title, author, id: uuid() }]);
+	};
+
+	const removeBook = (id) => {
+		setBooks(books.filter(() => books.id !== id));
+	};
+
+	<BookContext.Provider value={{ books, addBook, removeBook }}>
+		{props.children}
+	</BookContext.Provider>;
 };
+
+export default BookContextProvider;
