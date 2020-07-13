bea8d41fa8a12b3f4f7fba0296f70e87a375c282
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-07-13 06:03:17 -0700

    Downloaded uuid

--- hooks-app/src/components/SongList.js
+++ hooks-app/src/components/SongList.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
 
 const SongList = () => {
+	const [songs, setSongs] = useState([
+		{ title: 'almost home', id: 1 },
+		{ title: 'memory gospel', id: 2 },
+		{ title: 'this wild darkness', id: 3 },
+	]);
+
+	const addSong = () => {
+		setSongs([...songs, { title: 'new song', id: uuid() }]);
+	};
+
 	return (
 		<div>
 			<div className='song-list'>
 				<ul>
-					<li>This wild darkness</li>
-					<li>Memory gospel</li>
+					{songs.map((song) => {
+						return <li key={song.id}>{song.title}</li>;
+					})}
 				</ul>
+				<button onClick={addSong}>Add a song</button>
 			</div>
 		</div>
 	);
